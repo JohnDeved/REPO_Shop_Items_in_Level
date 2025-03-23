cbbd112fe0c95ca619f70c771847b7244d4bc294
undefined <johann@objekt.stream>
2025-03-23 16:12:53 +0100

    fix bookshelf (and other) spawns

--- Plugin.cs
+++ Plugin.cs
@@ -9,6 +9,8 @@ using UnityEngine;
 using Photon.Pun;
 namespace REPO_Shop_Items_in_Level;
 
+public class SwitchPlayerUpgradeTracker: MonoBehaviour {}
+
 [BepInPlugin(MyPluginInfo.PLUGIN_GUID, MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION)]
 [BepInProcess("REPO.exe")]
 public class Plugin : BaseUnityPlugin
@@ -60,6 +62,8 @@ public class Plugin : BaseUnityPlugin
     private static bool ShouldReplaceValuable(ValuableVolume volume, out SemiFunc.itemType? itemType)
     {
         itemType = null;
+        if (HasValuablePropSwitch(volume)) return false;
+
         switch (volume.VolumeType)
         {
             // tiny space = item upgrades
@@ -73,10 +77,20 @@ public class Plugin : BaseUnityPlugin
         }
     }
 
+
+    private static bool HasValuablePropSwitch(ValuableVolume volume)
+    {
+        return volume.transform.GetComponentInParent<ValuablePropSwitch>() != null;
+    }
+
     [HarmonyPatch(typeof(ValuableDirector), "Spawn")]
     [HarmonyPrefix]
     public static bool ValuableDirector_Spawn_Prefix(ref GameObject _valuable, ValuableVolume _volume, string _path)
     {
+        // debug
+        // var field = typeof(EnemyDirector).GetField("debugNoVision", BindingFlags.NonPublic | BindingFlags.Instance);
+        // field.SetValue(EnemyDirector.instance, true);
+
         // check if we should replace the valuable
         if (!ShouldReplaceValuable(_volume, out var itemType)) return true;
 
@@ -100,4 +114,34 @@ public class Plugin : BaseUnityPlugin
         Logger.LogInfo($"ValuableDirector spawning: {_valuable.name} with volume {_volume.name} at path {_path}");
         return true;
     }
+
+    // todo: check if all players have the mod installed so we can add the tracker component
+    private static void AddSwitchTrackerComponent(ValuableVolume volume)
+    {
+        var switches = volume.transform.GetComponentsInParent<ValuablePropSwitch>(true);
+
+        foreach (var switchObj in switches)
+        {
+            Logger.LogInfo($"Switch: {switchObj.gameObject.name}");
+            switchObj.gameObject.AddComponent<SwitchPlayerUpgradeTracker>();
+        }
+    }
+
+    // dead code
+    // [HarmonyPatch(typeof(ValuablePropSwitch), nameof(ValuablePropSwitch.Setup))]
+    // [HarmonyPostfix]
+    public static void ValuablePropSwitch_Setup_Posfix(ValuablePropSwitch __instance)
+	{
+        FieldInfo setupCompleteField = typeof(ValuablePropSwitch).GetField("SetupComplete", BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic);
+        bool SetupComplete = (bool)setupCompleteField.GetValue(__instance);
+        if (!SetupComplete) return;
+
+		var switchPlayerUpgradeComponent = __instance.gameObject.GetComponent<SwitchPlayerUpgradeTracker>();
+		if (!switchPlayerUpgradeComponent) return;
+		
+        __instance.PropParent.SetActive(value: false);
+        __instance.ValuableParent.SetActive(value: true);
+        
+        Logger.LogInfo($"ValuablePropSwitch found UpgradeTracker: {__instance.gameObject.name}");
+	}
 }
